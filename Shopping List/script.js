78ec727513bed750fa269a90f9c8785a64c06a34
aventador2018 <zl272@njit.edu>
2018-04-01 10:10:24 -0400

    Exercise
    1. If you click on the list item, it toggles the .done class on and off.
    
    2. Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
    
    3. When adding a new list item, it automatically adds the delete button next to it.

--- Shopping List/script.js
+++ Shopping List/script.js
@@ -1,7 +1,6 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var myLiClasses = document.getElementsByTagName("li").classList;
 var li = document.getElementsByTagName("li");
 var dlt = document.getElementsByClassName("delete");
 
@@ -9,28 +8,29 @@ function inputLength() {
 	return input.value.length;
 }
 
-function toggleDone() {
-	if (myLiClasses.contains("done")) {
-		myLiClasses.remove("done");
-	} else {
-		myLiClasses.add("done");
-	}
+function deleteItem() {
+	this.parentElement.remove();
 }
 
-function deleteItem() {
-	ul.removeChild("li");
+function toggleDone() {
+	this.classList.toggle("done");
 }
 
 function createListElement() {
 	var li = document.createElement("li");
     var dltbtn = document.createElement("button");
     dltbtn.appendChild(document.createTextNode("Delete"))
-	li.appendChild(document.createTextNode(input.value));
+	//li.appendChild(document.createTextNode(input.value));
+	li.innerText = input.value;
     li.appendChild(dltbtn);
 	ul.appendChild(li);
 	input.value = "";
 
-	dltbtn.addEventListener("click", deleteItem);
+    dltbtn.addEventListener("click", deleteItem);
+
+    li.addEventListener("click", function () {
+        this.classList.toggle("done")
+    });
 }
 
 function addListAfterClick() {
@@ -49,6 +49,13 @@ button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
+for (var i = 0; i < li.length; i++) {
+	li[i].addEventListener("click", toggleDone);
+}
+
+for (var i = 0; i < dlt.length; i++) {
+    dlt[i].addEventListener("click", deleteItem);
+}
 //dlt.addEventListener("click", alert("blablabla"));
 
 //dlt.addEventListener("click", deleteItem);
